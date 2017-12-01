410a5ae890d7df55df1316f6d1794f7610c615b2
hongruichen <chraac@gmail.com>
2017-12-01 15:24:32 +0800

    work!!

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 //
 //  main.cpp
-//  jscore.test
+//  lua.test
 //
 //  Created by hongruichen on 2017/11/27.
 //  Copyright © 2017年 hongruichen. All rights reserved.
@@ -8,9 +8,12 @@
 
 #include <iostream>
 #include <vector>
-#include <JavaScriptCore/JavaScriptCore.h>
+#include "lua.h"
+#include "lualib.h"
+#include "lauxlib.h"
 using namespace std;
 
+typedef lua_State* lua_State_Ptr;
 
 namespace
 {
@@ -18,63 +21,21 @@ namespace
     class Allocator {};
     
     template<>
-    class Allocator<JSGlobalContextRef>
+    class Allocator<lua_State_Ptr>
     {
     public:
-        typedef JSGlobalContextRef ObjectType;
+        typedef lua_State_Ptr ObjectType;
         
-        static void unref(JSContextRef, ObjectType ctx)
+        static ObjectType alloc() 
         {
-            JSGlobalContextRelease(ctx);
+            return luaL_newstate();
+        }
+
+        static void dealloc(ObjectType state)
+        {
+            lua_close(state);
         }
     };
-    
-    template<>
-    class Allocator<JSClassRef>
-    {
-    public:
-        typedef JSClassRef ObjectType;
-        
-        static void unref(JSContextRef, ObjectType clz)
-        {
-            JSClassRelease(clz);
-        }
-    };
-    
-    template<>
-    class Allocator<JSStringRef>
-    {
-    public:
-        typedef JSStringRef ObjectType;
-        
-        static void unref(JSContextRef, ObjectType obj)
-        {
-            JSStringRelease(obj);
-        }
-    };
-    
-    template<>
-    class Allocator<JSValueRef>
-    {
-    public:
-        typedef JSValueRef ObjectType;
-        
-        static void ref(JSContextRef ctx, ObjectType obj)
-        {
-            JSValueProtect(ctx, obj);
-        }
-        
-        static void unref(JSContextRef ctx, ObjectType obj)
-        {
-            if (ctx)
-            {
-                JSValueUnprotect(ctx, obj);
-            }
-        }
-    };
-    
-    template<>
-    class Allocator<JSObjectRef> : public Allocator<JSValueRef> {};
     
     template <typename _Ty, typename _Allocator = Allocator<_Ty>>
     class Handler
@@ -97,7 +58,7 @@ namespace
         {
             if (m_object)
             {
-                Allocator::unref(nullptr, m_object);
+                Allocator::dealloc(nullptr, m_object);
             }
         }
         
@@ -120,221 +81,200 @@ namespace
         
     protected:
         ObjectType m_object;
+        lua_State_Ptr m_state;
         
     private:
         Handler(const Handler&) = delete;
         void operator=(const Handler&) = delete;
     };
-    
-    template<typename _Ty>
-    class HandlerWithContext : public Handler<_Ty>
+
+    template <>
+    class Handler<lua_State_Ptr>
     {
     public:
-        typedef _Ty ObjectType;
-        typedef Handler<ObjectType> SuperClass;
-        typedef typename SuperClass::Allocator Allocator;
-        
-        HandlerWithContext(JSContextRef ctx, ObjectType value)
-        : Handler<ObjectType>(value)
-        , m_context(ctx)
+        typedef lua_State_Ptr ObjectType;
+        typedef Allocator<lua_State_Ptr> Allocator;
+
+        Handler()
         {
-            Allocator::ref(ctx, value);
-            SuperClass::m_object = value;
+            m_object = Allocator::alloc();
         }
-        
-        HandlerWithContext(HandlerWithContext &&handler)
-        : Handler<ObjectType>(nullptr)
+
+        Handler(Handler &&handler)
         {
             (*this) = std::move(handler);
         }
-        
-        ~HandlerWithContext()
+
+        virtual ~Handler()
         {
-            if (SuperClass::m_object)
+            if (m_object)
             {
-                Allocator::unref(m_context, SuperClass::m_object);
+                Allocator::dealloc(m_object);
             }
         }
-        
-        void operator=(HandlerWithContext &&handler)
+
+        ObjectType Get()const
         {
-            *static_cast<SuperClass*>(this) = std::move(handler);
-            m_context = handler.m_context;
-            handler.m_context = nullptr;
+            return m_object;
         }
-        
+
+        void operator=(Handler &&handler)
+        {
+            m_object = handler.m_object;
+            handler.m_object = nullptr;
+        }
+
+        operator ObjectType()const
+        {
+            return Get();
+        }
+
+
     protected:
-        JSContextRef m_context;
-        
+        ObjectType m_object;
+
     private:
-        HandlerWithContext(const HandlerWithContext&) = delete;
-        void operator=(const HandlerWithContext&) = delete;
+        Handler(const Handler&) = delete;
+        void operator=(const Handler&) = delete;
     };
-    
-    auto MakeString(const char *sz)
-    {
-        return Handler<JSStringRef>(JSStringCreateWithUTF8CString(sz));
-    }
-    
-    template<typename _Ty>
-    auto MakeHandlerWithContext(JSContextRef ctx, _Ty value)
-    {
-        return HandlerWithContext<_Ty>(ctx, value);
-    }
+
+    const char *kClassName = "TestClass";
 }
 
 
 int main(int argc, const char * argv[]) {
-    std::cout << "jscore.test.context.create" << endl;
-    auto ctx = Handler<JSGlobalContextRef>(JSGlobalContextCreate(nullptr));
-    
-    std::cout << "jscore.test.start" << endl;
-    JSValueRef exception = nullptr;
-    {
-        // call js function
-        auto scriptString = MakeString(R"--(var add = function(a, b){return a+b;})--");
-        JSEvaluateScript(ctx, scriptString, nullptr, nullptr, 0, &exception);
-        {
-            auto funcName = MakeString("add");
-            auto obj = MakeHandlerWithContext(ctx, JSObjectGetProperty(ctx, JSContextGetGlobalObject(ctx), funcName, &exception));
-            JSGarbageCollect(ctx);
-            auto func =  MakeHandlerWithContext(ctx, JSValueToObject(ctx, obj, &exception));
-            JSGarbageCollect(ctx);
-            JSValueRef args[2];
-            args[0] = JSValueMakeNumber(ctx, 1.0);
-            args[1] = JSValueMakeNumber(ctx, 2.0);
-            JSValueRef ret = JSObjectCallAsFunction(ctx, func, nullptr, 2, args, &exception);
-            cout << "call js function add(1, 2), return " << JSValueToNumber(ctx, ret, &exception) << endl;
-        }
+    std::cout << "lua.test.state.create" << endl;
+
+    auto state = Handler<lua_State_Ptr>();
+    const luaL_Reg loadedlibs[] = {
+        { "_G", luaopen_base },
+        { LUA_COLIBNAME, luaopen_coroutine },
+        { LUA_TABLIBNAME, luaopen_table },
+        { LUA_OSLIBNAME, luaopen_os },
+        { LUA_STRLIBNAME, luaopen_string },
+        { LUA_MATHLIBNAME, luaopen_math },
+        { LUA_UTF8LIBNAME, luaopen_utf8 },
+        { LUA_DBLIBNAME, luaopen_debug },
+        { NULL, NULL }
+    };
+
+    const luaL_Reg *lib;
+    for (lib = loadedlibs; lib->func; lib++) {
+        luaL_requiref(state, lib->name, lib->func, 1);
+        lua_pop(state, 1);
     }
+    
+    cout << "line: " << __LINE__ << "lua.stack.count" << lua_gettop(state) << endl;
+    std::cout << "lua.test.start" << endl;
+    {
+        // call lua function
+        luaL_dostring(state, R"--(function add(a, b) return a+b end)--");
+        lua_getglobal(state, "add");
+        lua_pushnumber(state, 1);
+        lua_pushnumber(state, 2);
+        lua_call(state, 2, 1);
+        double result = lua_tonumber(state, lua_gettop(state));
+        lua_pop(state, 1);
+        cout << "call lua function add(1, 2), return " << result << endl;
+    }
+    cout << "line: " << __LINE__ << "lua.stack.count" << lua_gettop(state) << endl;
     {
         // regist callback function
-        auto callback = [](JSContextRef ctx,
-                           JSObjectRef function,
-                           JSObjectRef thisObject,
-                           size_t argumentCount,
-                           const JSValueRef arguments[],
-                           JSValueRef* exception) -> JSValueRef
+        lua_CFunction callback = [](lua_State_Ptr l) -> int
         {
-            cout << "js function callback, test(";
-            for (size_t i = 0; i < argumentCount; ++i)
+            size_t count = lua_gettop(l);
+            cout << "lua function callback, test(";
+            for (size_t i = 1; i <= count; ++i)
             {
                 if (i > 0)
                 {
                     cout << ", ";
                 }
-                auto &arg = arguments[i];
-                JSType type = JSValueGetType(ctx, arg);
-                switch (type)
+
+                switch (lua_type(l, i))
                 {
-                    case kJSTypeNull:
+                    case LUA_TNONE:
+                    case LUA_TNIL:
                         cout << "null";
                         break;
-                    case kJSTypeBoolean:
-                        cout << (JSValueToBoolean(ctx, arg)? "true": "false");
+                    case LUA_TBOOLEAN:
+                        cout << (lua_toboolean(l, i)? "true": "false");
                         break;
-                    case kJSTypeNumber:
-                        cout << JSValueToNumber(ctx, arg, exception);
+                    case LUA_TNUMBER:
+                        cout << lua_tonumber(l, i);
                         break;
                 }
             }
             cout << ")" << endl;
-            return JSValueMakeUndefined(ctx);
+            lua_pop(l, count);
+            return 0;
         };
-        
-        auto funcName = MakeString("test");
-        JSObjectRef func = JSObjectMakeFunctionWithCallback(ctx, funcName, callback);
-        JSGarbageCollect(ctx);
-        JSObjectSetProperty(ctx, JSContextGetGlobalObject(ctx), funcName, func, kJSPropertyAttributeNone, &exception);
-        
-        auto scriptString = MakeString(R"--(test(1, false);)--");
-        JSEvaluateScript(ctx, scriptString, nullptr, nullptr, 0, &exception);
+
+        lua_pushcfunction(state, callback);
+        lua_setglobal(state, "test");
+
+        luaL_dostring(state, R"--(test(1, false))--");
     }
+    cout << "line: " << __LINE__ << "lua.stack.count" << lua_gettop(state) << endl;
     {
-        // js class test
-        auto Constructor = [] (JSContextRef ctx,
-                               JSObjectRef object,
-                               size_t argumentCount,
-                               const JSValueRef arguments[],
-                               JSValueRef* exception) -> JSObjectRef
+        // lua class test
+        lua_CFunction Constructor = [](lua_State_Ptr l) -> int
         {
-            long data = 123456;
-            JSObjectSetPrivate(object, reinterpret_cast<void*>(data));
-            cout << "TestClass.constructor.call, data = " << data << endl;
-            return object;
+            auto ptr = (long*)lua_newuserdata(l, sizeof(long));
+            *ptr = 123456;
+            int newTable = lua_gettop(l);
+            luaL_getmetatable(l, kClassName);
+            lua_setmetatable(l, newTable);
+            cout << "TestClass.constructor.call, data = " << *ptr << endl;
+            return 1;
         };
         
-        auto Destructor = [] (JSObjectRef object)
+        lua_CFunction Destructor = [](lua_State_Ptr l) -> int
         {
-            auto data = (long)JSObjectGetPrivate(object);
-            cout << "TestClass.destructor.call, data = " << data << endl;
+            if (lua_type(l, 1) == LUA_TUSERDATA)
+            {
+                auto ptr = (long*)lua_touserdata(l, 1);
+                cout << "TestClass.destructor.call, data = " << *ptr << endl;
+            }
+            else
+            {
+                cout << "TestClass.destructor.call, data.mismatch" << endl;
+            }
+            return 0;
         };
         
-        auto TestFunc = [] (JSContextRef ctx,
-                            JSObjectRef function,
-                            JSObjectRef thisObject,
-                            size_t argumentCount,
-                            const JSValueRef arguments[],
-                            JSValueRef* exception) -> JSValueRef
+        lua_CFunction TestFunc = [](lua_State_Ptr l) -> int
         {
-            cout << "TestClass.testFunc.call" << endl;
-            return JSValueMakeBoolean(ctx, true);
+            if (lua_type(l, 1) == LUA_TUSERDATA)
+            {
+                auto ptr = (long*)lua_touserdata(l, 1);
+                cout << "TestClass.testFunc.call, data = " << *ptr << endl;
+            }
+            else
+            {
+                cout << "TestClass.testFunc.call, data.mismatch" << endl;
+            }
+            lua_pushboolean(l, true);
+            return 1;
         };
         
-        auto getter = [] (JSContextRef ctx,
-                          JSObjectRef object,
-                          JSStringRef propertyName,
-                          JSValueRef* exception) -> JSValueRef
-        {
-            vector<char> buffer(JSStringGetMaximumUTF8CStringSize(propertyName));
-            JSStringGetUTF8CString(propertyName, buffer.data(), buffer.size());
-            cout << "TestClass." << buffer.data() << ".getter.call" << endl;
-            return JSValueMakeNumber(ctx, 1.23456);
-        };
-        
-        auto setter = [] (JSContextRef ctx,
-                          JSObjectRef object,
-                          JSStringRef propertyName,
-                          JSValueRef value,
-                          JSValueRef* exception) -> bool
-        {
-            vector<char> buffer(JSStringGetMaximumUTF8CStringSize(propertyName));
-            JSStringGetUTF8CString(propertyName, buffer.data(), buffer.size());
-            cout << "TestClass." << buffer.data() << ".setter.call.";
-            cout << JSValueToNumber(ctx, value, exception) << endl;
-            return true;
-        };
-        
-        JSClassDefinition clzDef = kJSClassDefinitionEmpty;
-        static JSStaticFunction staticFuncs[] = {
-            { "testFunc", TestFunc, kJSPropertyAttributeReadOnly | kJSPropertyAttributeDontDelete },
-            { 0, 0, 0 }
-        };
-        
-        static JSStaticValue staticValues[] = {
-            { "testValue", getter, setter, kJSPropertyAttributeDontDelete },
-            { 0, 0, 0, 0 }
-        };
-        
-        clzDef.className = "TestClass";
-        clzDef.attributes = kJSClassAttributeNone;
-        clzDef.staticFunctions = staticFuncs;
-        clzDef.staticValues = staticValues;
-        clzDef.finalize = Destructor;
-        clzDef.callAsConstructor = Constructor;
-        
-        {
-            auto clz = Handler<JSClassRef>(JSClassCreate(&clzDef));
-            auto clzName = MakeString("TestClass");
-            JSObjectSetProperty(ctx, JSContextGetGlobalObject(ctx), clzName, JSObjectMake(ctx, clz, nullptr), kJSPropertyAttributeNone, &exception);
-            JSGarbageCollect(ctx);
-        }
-        
-        auto scriptString = MakeString(R"--(var tst = new TestClass();tst.testValue = 789;var t = tst.testValue;tst.testFunc();)--");
-        JSEvaluateScript(ctx, scriptString, nullptr, nullptr, 0, &exception);
+        luaL_newmetatable(state, kClassName);
+        lua_pushcfunction(state, Destructor);
+        lua_setfield(state, -2, "__gc");
+
+        lua_pushcfunction(state, TestFunc);
+        lua_setfield(state, -2, "testFunc");
+
+        lua_pushcfunction(state, Constructor);
+        lua_setfield(state, -2, "new");
+
+        lua_setglobal(state, kClassName);
+
+        luaL_dostring(state, R"--(obj = TestClass.new(); obj:testFunc())--");
     }
-    std::cout << "jscore.test.end" << endl;
-    
-    std::cout << "jscore.test.context.destroy" << endl;
+    std::cout << "lua.test.end" << endl;
+    cout << "line: " << __LINE__ << "lua.stack.count" << lua_gettop(state) << endl;
+
+    std::cout << "lua.test.state.destroy" << endl;
     return 0;
 }
